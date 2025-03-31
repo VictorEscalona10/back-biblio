2bdbaeaa874a00b5fb838054b890518c3107f7e5
VictorAdmin <victorescalona2006@gmail.com>
2025-03-31 14:44:20 -0400

    asegurando

--- backend/src/controllers/book.controller.js
+++ backend/src/controllers/book.controller.js
@@ -28,6 +28,7 @@ export const getBook = async (req, res) => {
 };
 
 export const createBook = async (req, res) => {
+    const upload = multer({ storage: multer.memoryStorage() }); 
     try {
         const { title, year, author, file_link } = req.body;
         const { data, error } = await supabase.from('books').insert({ title, year, author, file_link });
