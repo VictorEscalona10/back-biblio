b4b20c567dedf05e5d39de45cdc8cee12700c5f4
VictorAdmin <victorescalona2006@gmail.com>
2025-03-31 21:57:49 -0400

    listo la coneccion de add_book admin al back

--- frontend/views/add_books_admin.py
+++ frontend/views/add_books_admin.py
@@ -7,6 +7,7 @@ def add_book_page_admin(page: ft.Page, body_column: ft.Column, user_email: str):
     body_column.controls.clear()
     
     title_field = ft.TextField(label="Título", autofocus=True, width=400)
+    message_label = ft.Text(value="", size=20, color=ft.colors.GREEN)
     author_field = ft.TextField(label="Autor", width=400)
     year_field = ft.TextField(label="Año", width=400)
     link_field = ft.TextField(label="Enlace", width=400)
@@ -19,19 +20,31 @@ def add_book_page_admin(page: ft.Page, body_column: ft.Column, user_email: str):
             title_field.update()
             return
         
-        # Aquí iría tu llamada a POST /books
-        new_book = {
-            "title": title_field.value,
-            "author": author_field.value,
-            "year": year_field.value,
-            "link": link_field.value
-        }
+        response = requests.post(
+            f"{BASE_URL}/books",
+            json={
+                "title": title_field.value,
+                "author": author_field.value,
+                "year": year_field.value,
+                "link": link_field.value
+            }
+        )
+
+        if response.status_code == 201:
+            message_label.value = "Libro agregado correctamente"
+        else:
+            message_label.value = "Error al agregar el libro"
         
-        page.snack_bar = ft.SnackBar(ft.Text("Libro agregado correctamente"))
+        title_field.value = ""
+        author_field.value = ""
+        year_field.value = ""
+        link_field.value = ""
+        page.update()
     
     text_fields = body_column.controls.extend([
         ft.Text("Agregar Nuevo Libro", size=24, weight=ft.FontWeight.BOLD),
         title_field,
+        message_label,
         author_field,
         year_field,
         link_field,
